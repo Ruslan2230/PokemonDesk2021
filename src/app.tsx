06fa15f9ef650a0bed326fb0af28ed2616337566
pandakung-fu <gritsak5@gmail.com>
2020-11-03 17:56:34 +0200

    homework-2

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import cn from "classnames";
+
+import "./custom.css";
+// import s  from "./App.modules.scss";
+
+const App = () => {
+  return <div >This is App component !</div>;
+};
+
+export default App;
