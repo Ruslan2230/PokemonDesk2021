aeb88910556f02cfad6484569ffbc0d35ceedf8c
pandakung-fu <gritsak5@gmail.com>
2020-11-18 18:10:50 +0200

    homework-7

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import PokemonCard from '../../components/PokemonCard';
-
-import usePokemons from '../../hooks/usePokemons';
+import usePokemons from '../../hooks/useData';
 import { isEmpty } from '../../helpers';
 import s from './Pokedex.module.scss';
 
 const PokedexPage = () => {
-  const { data, isLoading, isError } = usePokemons();
+  const [serchValue, setSerchValue] = useState('');
+  const [query, setQuery] = useState({});
+  // const query = useMemo(() => ({ name: serchValue }), [serchValue]);
+
+  const { data, isLoading, isError } = usePokemons('getPokemons', query, [serchValue]);
+
+  const handleSerchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSerchValue(e.target.value);
+    setQuery((s) => ({
+      ...s,
+      name: e.target.value,
+    }));
+  };
 
   if (isLoading) {
     return <div className={s.root}>Loading ...</div>;
@@ -19,12 +29,13 @@ const PokedexPage = () => {
 
   return (
     <div className={s.root}>
+      <input type="text" value={serchValue} onChange={handleSerchChange} />
       <div>Total {data.total}</div>
 
       {!isEmpty(data) &&
         data.pokemons.map((item: any) => (
           <>
-            <PokemonCard />
+            <p>{item.name}</p>
           </>
         ))}
     </div>
